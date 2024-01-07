40241d0a449a5333ff0485a3e537bc54a646d6b1
Zedespook <26501287+Xarithma@users.noreply.github.com>
2024-01-07 03:43:59 +0100

    Add acceleration/deceleration to player

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody3D
 
-const SPEED: float = 5.0
-const JUMP_VELOCITY: float = 4.5
+@export var JUMP_VELOCITY: float = 12.0
+@export var FRICTION: float = 5.0
+@export var ACCELERATION: float = 4.0
+@export var MAX_SPEED: float = 15.0
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -13,17 +15,32 @@ func _ready() -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		var mouse_motion: InputEventMouseMotion = event as InputEventMouseMotion
 		rotate_y(deg_to_rad(mouse_motion.relative.x * -0.1))
 		head.rotate_x(deg_to_rad(mouse_motion.relative.y * -0.1))
 
 
+func _input(event):
+	if event.is_action_pressed("pause"):
+		var new_mouse_mode: int = (
+			Input.MOUSE_MODE_CAPTURED
+			if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE
+			else Input.MOUSE_MODE_VISIBLE
+		)
+		Input.set_mouse_mode(new_mouse_mode)
+
+
 func _physics_process(delta: float) -> void:
+	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
+		return
+
+		
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var input_dir: Vector2 = Input.get_vector(
@@ -32,7 +49,13 @@ func _physics_process(delta: float) -> void:
 
 	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
-	velocity.x = direction.x * SPEED
-	velocity.z = direction.z * SPEED
+	var target_velocity: Vector3 = direction * MAX_SPEED
+	var velocity_change: Vector3 = target_velocity - velocity
+	velocity_change.y = 0
+
+	var impulse: Vector3 = velocity_change * ACCELERATION * delta
+	velocity += impulse
+
+	velocity = velocity.lerp(Vector3.ZERO, FRICTION * delta)
 
 	move_and_slide()
