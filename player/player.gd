41f47ca36039db7a4af28746aed1cd93a4d2e50a
Zedespook <26501287+Xarithma@users.noreply.github.com>
2024-01-09 08:28:13 +0100

    Update player movement and camera controls (Thanks to LegionGames)

--- player/player.gd
+++ player/player.gd
@@ -1,13 +1,23 @@
 extends CharacterBody3D
 
-@export var JUMP_VELOCITY: float = 12.0
-@export var FRICTION: float = 5.0
-@export var ACCELERATION: float = 4.0
-@export var MAX_SPEED: float = 15.0
+const JUMP_VELOCITY: float = 4.5
+const AIR_ACCELERATION: float = 3.0
+const MOUSE_SENSITIVITY: float = 0.01
+const WALK_SPEED: float = 5.0
+const RUN_SPEED: float = 8.0
+var speed: float = WALK_SPEED
 
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+const BOB_FREQUENCY: float = 2.0
+const BOB_AMPLITUDE: float = 0.05
+var bob_timer: float = 0.0
+
+const BASE_FOV: float = 75.0
+const FOV_CHANGE: float = 1.5
+
+var gravity: float = 9.8
 
 @onready var head: Node3D = $Head
+@onready var camera: Camera3D = $Head/View
 
 
 func _ready() -> void:
@@ -16,9 +26,9 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		var mouse_motion: InputEventMouseMotion = event as InputEventMouseMotion
-		rotate_y(deg_to_rad(mouse_motion.relative.x * -0.1))
-		head.rotate_x(deg_to_rad(mouse_motion.relative.y * -0.1))
+		head.rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		camera.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
 
 func _input(event):
@@ -32,30 +42,46 @@ func _input(event):
 
 
 func _physics_process(delta: float) -> void:
-	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
-		return
-
-		
-
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_pressed("run"):
+		speed = RUN_SPEED
+	else:
+		speed = WALK_SPEED
+
 	var input_dir: Vector2 = Input.get_vector(
 		"move_left", "move_right", "move_forward", "move_back"
 	)
 
-	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction: Vector3 = -(
+		(head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	)
 
-	var target_velocity: Vector3 = direction * MAX_SPEED
-	var velocity_change: Vector3 = target_velocity - velocity
-	velocity_change.y = 0
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = 0.0
+			velocity.z = 0.0
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * AIR_ACCELERATION)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * AIR_ACCELERATION)
 
-	var impulse: Vector3 = velocity_change * ACCELERATION * delta
-	velocity += impulse
+	bob_timer += velocity.length() * float(is_on_floor()) * delta
+	camera.transform.origin = _bob_and_sway_player_head(bob_timer)
 
-	velocity = velocity.lerp(Vector3.ZERO, FRICTION * delta)
+	var new_fov: float = BASE_FOV + FOV_CHANGE * clamp(velocity.length(), 0.5, RUN_SPEED * 2)
+	camera.fov = lerp(camera.fov, new_fov, delta * 8.0)
 
 	move_and_slide()
+
+
+func _bob_and_sway_player_head(timer: float) -> Vector3:
+	var bob: float = sin(timer * BOB_FREQUENCY) * BOB_AMPLITUDE
+	var sway: float = cos(timer * BOB_FREQUENCY * 0.5) * BOB_AMPLITUDE
+	return Vector3(sway, bob, 0)
